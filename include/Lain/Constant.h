22cfb7c2b88b3c3745f1b008894f63f75279b989
Julien Ruffin <jruffin@gmail.com>
2017-02-18 01:34:02 +0100

    Updated copyright year

--- include/Lain/Constant.h
+++ include/Lain/Constant.h
@@ -1,7 +1,7 @@
 /*
  * Serialization Experiments Lain
  *
- * Copyright (C) 2016 Julien Ruffin and project contributors
+ * Copyright (C) 2016-2017 Julien Ruffin and project contributors
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
